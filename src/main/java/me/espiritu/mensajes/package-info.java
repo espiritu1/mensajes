1eed4c4d8f659c0e40bca4b29429ad227ef63791
FernandoEspiritu <tiran.sc.f@gmail.com>
2021-04-16 17:04:55 -0500

    repositorio funcionando mensajes 2

--- src/main/java/me/espiritu/mensajes/package-info.java
+++ src/main/java/me/espiritu/mensajes/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2021.04.16 a las 11:45:33 AM CDT 
+// Generado el: 2021.04.16 a las 05:01:46 PM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://espiritu.me/mensajes", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
